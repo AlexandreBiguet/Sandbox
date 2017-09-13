0ff0a68c600e7217eb1aa5e6bc6ec03fec9add46
ABiguet <alexandre.biguet@gmail.com>
2017-09-13 22:01:41 +0200

    add cmakelists trees and fake files to test compil chain

---
+++ src/snmpp/fake/fake.cpp
@@ -0,0 +1,22 @@
+// fake.cpp --- 
+// 
+// Filename: fake.cpp
+// Author: Alexandre BIGUET
+// Created: Mer sep 13 21:09:00 2017 (+0200)
+// Last-Updated: Mer sep 13 21:09:35 2017 (+0200)
+//           By: Alexandre BIGUET
+//     Update #: 4
+// 
+// snmpp
+// 
+
+namespace snmpp { namespace fake {
+
+double fake( double x ) {
+    return x;
+}
+
+}} // namespace snmpp::fake
+
+// 
+// fake.cpp ends here
---
+++ src/snmpp/fake/fake.hpp
@@ -0,0 +1,20 @@
+// fake.hpp --- 
+// 
+// Filename: fake.hpp
+// Author: Alexandre BIGUET
+// Created: Mer sep 13 21:09:41 2017 (+0200)
+// Last-Updated: Mer sep 13 21:10:01 2017 (+0200)
+//           By: Alexandre BIGUET
+//     Update #: 2
+// 
+// snmpp
+// 
+
+namespace snmpp { namespace fake {
+
+double fake( double x );
+
+}} // namespace snmpp::fake
+
+// 
+// fake.hpp ends here
