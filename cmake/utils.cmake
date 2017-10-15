5294a2d61ccad709bd5abf38c73c1ed49e852e6c
ABiguet <alexandre.biguet@gmail.com>
2017-10-15 23:58:05 +0200

    remove useless functions and update install files

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -14,13 +14,14 @@
 ## This function creates a list of $<TARGET_OBJECTS:${ojb_i}> from a list of
 ## target objects #_inputList
 ## The function creates a TARGET_OBJECT_LIST list at parent scope
-function( FillTargetObjectList _inputList )
+macro( FillTargetObjectList _inputList _outputList)
 
     foreach( obj ${_inputList})
         get_target_property(_type "${obj}" TYPE)
 
         if (_type STREQUAL "OBJECT_LIBRARY")
-            list( APPEND _list "$<TARGET_OBJECTS:${obj}>")
+            #list( APPEND _list "$<TARGET_OBJECTS:${obj}>")
+            list( APPEND ${_outputList} "$<TARGET_OBJECTS:${obj}>")
         else()
             message(FATAL_ERROR
                     "Some object in list is not of type OBJECT_LIBRARIES")
@@ -28,9 +29,9 @@ function( FillTargetObjectList _inputList )
 
     endforeach()
 
-    set ( TARGET_OBJECTS_LIST ${_list} PARENT_SCOPE )
+    #set ( TARGET_OBJECTS_LIST ${_list} PARENT_SCOPE )
 
-endfunction()
+endmacro()
 
 
 ################################################################################
@@ -88,36 +89,8 @@ function( PrintPackagesInformation _list )
 endfunction()
 
 ################################################################################
-## This function takes a list in input and removes from the list the
-## item that match _dir. _verbose can be TRUE or FALSE. If true, then
-## the function talks
-##
-## Output: this functions defines a variables NEW_LIST which contain
-## the output list. This variable can be used is the parent scope
-## (i.e. where this function were called)
-
-function ( glob_recurse_without_dir _list _dir _verbose )
-
-  foreach( itr ${_list} )
-    if ( "${_verbose}" )
-      message( STATUS "iteration: ${itr}" )
-    endif()
-
-    if ( "${itr}" MATCHES "(.*)${_dir}(.*)" )
-      if ( "${_verbose}" )
-	message( STATUS "Item ${itr} is part of ${_dir}: removed from list" )
-      endif()
-      list( REMOVE_ITEM _list ${itr} )
-    endif()
-  endforeach()
-  set( NEW_LIST ${_list} PARENT_SCOPE )
-endfunction()
-
-
-################################################################################
 ## This function installs the files in _list into _dest
-
-function ( install_files _list _dest _verbose)
+function ( InstallFilesInList _list _dest _verbose)
 
   ## test if _dest ends with a '/'
   string( LENGTH "${_dest}" leng )
@@ -148,40 +121,6 @@ function ( install_files _list _dest _verbose)
     install( FILES ${file} DESTINATION "${_dest}/${dir}" )
   endforeach()
 endfunction()
-
-################################################################################
-## This function installs the files in _list into _dest and removes
-## _rpath in the path of the files
-
-function( install_files_rpath _list _dest _rpath _verbose )
-  ## test if _dest ends with a '/'
-  string( LENGTH "${_dest}" leng )
-  math( EXPR begin "${leng}-1" )
-  string( SUBSTRING "${_dest}" "${begin}" "1" last_char )
-  
-  if ( "${last_char}" STREQUAL "/" )
-
-    if ( "${_verbose}" )
-      message( "in ${_dest} : last char is a '/' " )
-    endif()
-
-    string( SUBSTRING "${_dest}" 0 "${begin}" _dest )
-  endif()
-  
-  foreach( file ${_list} )
-    get_filename_component( dir ${file} DIRECTORY )
-    string( REPLACE "${CMAKE_SOURCE_DIR}/" "" dir "${dir}" )
-    string( REPLACE "${_rpath}" "" dir "${dir}" )
-    
-    if ( "${_verbose}" )
-      message( " ${file} will be installed to ${_dest}/${dir}" )
-      message ("" )
-    endif()
-    install( FILES ${file} DESTINATION "${_dest}/${dir}" )
-  endforeach()  
-endfunction()
-
-
 
 ######################################################################
 ### utils.cmake ends here
