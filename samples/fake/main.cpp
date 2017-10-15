62453506a488fd0e6d8c65a62c7e55e3b4d689a9
ABiguet <alexandre.biguet@gmail.com>
2017-10-15 23:39:45 +0200

    add sample fake test file

---
+++ samples/fake/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp.cpp 
+//  snmpp
+//
+//  Created on 15/10/2017 at 22:29.
+//  Copyright Alexandre Biguet © 2017. All rights reserved.
+//  Distributed under the MIT Licence
+//
+
+#include <iostream>
+
+//#include <snmpp/fake/fake.hpp>
+#include "fake.hpp"
+
+int main ( ){
+
+    double x = snmpp::fake::fake(10.0);
+    std::cout<<x<<std::endl;
+
+    return 0;
+}
+
+// main.cpp.cpp ends here
