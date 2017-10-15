8f9fb31a106748f4d818dfcb4dde8ecd24c7cfa1
ABiguet <alexandre.biguet@gmail.com>
2017-10-16 00:01:39 +0200

    update SetCompilerFlags -> is now a macro and check if all flags are available for the compiler before setting cmake cxx flags : TODO : check if there are built-in cmake command to add cxx flags

--- cmake/Flags.cmake
+++ cmake/Flags.cmake
@@ -10,7 +10,7 @@
 ## This functions sets the compiler flags depending on the options given at
 ## configuration time.
 
-function( SetCompilerFlags )
+macro( SetCompilerFlags )
 
     if( NO_WARNINGS AND CLANG_WEVERYTHING )
         message(FATAL_ERROR "NO_WARNINGS and CLANG_EVERYTHING options cannot "
@@ -22,15 +22,33 @@ function( SetCompilerFlags )
         CHECK_CXX_COMPILER_FLAG(-Weverything COMPILER_SUPPORTS_EVERYTHING )
 
         if ( COMPILER_SUPPORTS_EVERYTHING )
-            set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything" PARENT_SCOPE)
+            set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything" )
         endif()
 
     else()
+
         if ( NOT NO_WARNINGS )
-            set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Wextra" PARENT_SCOPE)
+
+            CHECK_CXX_COMPILER_FLAG( -W COMPILER_SUPPORTS_W )
+            CHECK_CXX_COMPILER_FLAG( -Wall COMPILER_SUPPORTS_WALL )
+            CHECK_CXX_COMPILER_FLAG( -Wextra COMPILER_SUPPORTS_WEXTRA )
+
+            if( COMPILER_SUPPORTS_W  )
+                set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W")
+            endif()
+
+            if( COMPILER_SUPPORTS_WALL )
+                set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+            endif()
+
+            if( COMPILER_SUPPORTS_WEXTRA )
+                set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra" )
+            endif()
+
         endif()
+
     endif()
 
-endfunction()
+endmacro()
 
 # Flags.cmake ends here
