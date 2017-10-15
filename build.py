effe28e454e2a4a118acecefa3b312050037196a
ABiguet <alexandre.biguet@gmail.com>
2017-10-15 23:39:13 +0200

    update build script

--- build.py
+++ build.py
@@ -2,17 +2,17 @@
 # coding: utf-8
 
 
-### build.py --- 
-## 
+### build.py ---
+##
 ## Filename: build.py
 ## Author: Alexandre BIGUET
 ## Created: Mer sep 13 21:16:33 2017 (+0200)
 ## Last-Updated: Mer sep 13 21:59:45 2017 (+0200)
 ##           By: Alexandre BIGUET
 ##     Update #: 38
-## 
+##
 ## snmpp
-## 
+##
 
 import subprocess as Sub
 import argparse as Arg
@@ -33,8 +33,9 @@ if sys.version_info.major < 3 and sys.version_info.minor < 5 :
 
 pwd_full = os.path.dirname(os.path.realpath(__file__))
 unobj = os.uname()
-default_build_dir = pwd_full + "/build-cmake/build_" + unobj.machine +"-"+unobj.sysname + unobj.release
-default_release_dir = pwd_full + "/build-cmake/release"
+default_base_dir = pwd_full + "/Build-cmake"
+default_build_dir = default_base_dir + "/build_" + unobj.machine +"-"+unobj.sysname + unobj.release
+default_release_dir = default_base_dir + "/release"
 
 def do_install(bdir,rdir):
 
@@ -43,7 +44,7 @@ def do_install(bdir,rdir):
 
     os.chdir(bdir)
     Sub.run(["cmake", pwd_full, "-DCMAKE_INSTALL_PREFIX="+rdir])
-    nproc = int( Sub.check_output(["getconf", "_NPROCESSORS_ONLN"]) )  
+    nproc = int( Sub.check_output(["getconf", "_NPROCESSORS_ONLN"]) )
     Sub.run(["make", "-j"+str(nproc), "install"])
 
 
@@ -51,19 +52,31 @@ parser = Arg.ArgumentParser()
 
 parser.add_argument("--build-dir", metavar="<path>", type=str, nargs='?',
                     default=default_build_dir,
-                    help="The path to the build directory. Default is build/build/")
+                    help="The path to the build directory. Default is Build-cmake/build/")
 
 parser.add_argument("--release-dir", metavar="<path>", type=str, nargs='?',
                     default=default_release_dir,
                     help="The path to the release dir, i.e. where you want to "
-                    "install the library. Default is build/release/")
+                    "install the library. Default is Build-cmake/release/")
+
+parser.add_argument("--clean",  action='store_true',
+                    help="remove build and install directory. Default is"
+                    " Build-cmake/build and Build-cmake/release")
 
 
 args = parser.parse_args()
 
 print(args)
 
+if( args.clean ) :
+    if( os.path.exists(default_base_dir) ):
+        print(" {} directory exists".format(default_base_dir))
+        Sub.run(["rm", "-rf", default_base_dir ])
+    else:
+        print(" Build-cmake/ directory does not exist: Nothing to clean ")
+    quit()
+
 do_install( args.build_dir, args.release_dir )
-        
+
 ######################################################################
 ### build.py ends here
