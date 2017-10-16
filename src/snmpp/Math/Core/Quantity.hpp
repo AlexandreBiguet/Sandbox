b0b0aab41bc1b17563cc1fadf3d09261de0e6df0
ABiguet <alexandre.biguet@gmail.com>
2017-10-16 22:32:30 +0200

    add Quantity implementation

--- src/snmpp/Math/Core/Quantity.hpp
+++ src/snmpp/Math/Core/Quantity.hpp
@@ -11,21 +11,38 @@
 /******************************************************************************
  * Description: 
  * ------------
+ * This class represents a general quantity
  ******************************************************************************/
 
 /******************************************************************************
  * Class Attributes:
- * ----------------- 
+ * -----------------
+ * _value : the actual value of the corresponding quantity
+ * _interval : the interval of definition of this quantity
  ******************************************************************************/
 
 /******************************************************************************
  * Public Methods: 
  * ---------------
+ * Constructors:
+ *    Quantity() :
+ *      value initialized to 0 and interval initialized to ]-inf;+inf[
+ *
+ *    Quantity(const Type &val) :
+ *      value initialized to val and interval init. to whole
+ *
+ *     Quantity(const Type &val, const Type &lower, const Type &upper) :
+ *      value init to val and interval init to [lower, upper]
+ *
+ * Getters and Setters -> see interface
+ *
+ * std::string toString() : debug only
  ******************************************************************************/
 
 /******************************************************************************
  * Private Methods:
  * ----------------
+ * None
  ******************************************************************************/
 
 /******************************************************************************
@@ -33,10 +50,101 @@
  * ----------------------
  ******************************************************************************/
 
-namespace  snmpp {
+#include <boost/numeric/interval.hpp>
+
+namespace  snmpp { namespace math {
+
+template < typename Type >
+class Quantity {
+
+  public:
+    template<typename T>
+    using IntervalType = boost::numeric::interval<T>;
+
+  private:
+    Type _value;
+    IntervalType<Type> _interval;
+
+  public:
+    Quantity();
+    explicit Quantity(const Type &val);
+    Quantity(const Type &val, const Type &lower, const Type &upper);
+
+    Type getValue() const;
+    void setValue(const Type &v);
+    void setLower(const Type &v);
+    void setUpper(const Type &v);
+    void setInterval(const Type &lower, const Type &upper);
+    Type getLower() const;
+    Type getUpper() const;
+    IntervalType<Type> getInterval() const;
+    std::string toString()const;
+};
 
 
+template< typename Type >
+Quantity<Type>::Quantity() : _value(static_cast<Type>(0)) {
+    _interval.set_whole();
+}
 
-} // namespace snmpp 
+template< typename Type >
+Quantity<Type>::Quantity(const Type &val) : _value(val) {
+    _interval.set_whole();
+}
+
+template< typename Type >
+Quantity<Type>::Quantity(const Type &val, const Type &lower, const Type &upper)
+    : _value(val), _interval(lower, upper)
+{}
+
+template <typename Type>
+Type Quantity<Type>::getValue() const {
+    return _value;
+}
+
+template <typename Type>
+void Quantity<Type>::setValue(const Type &v) {
+    _value = v;
+}
+
+template <typename Type>
+void Quantity<Type>::setLower(const Type &v) {
+    _interval.assign(v, _interval.upper() );
+}
+
+template <typename Type>
+void Quantity<Type>::setUpper(const Type &v) {
+    _interval.assign(_interval.lower(), v);
+}
+
+template <typename Type>
+void Quantity<Type>::setInterval(const Type &lower, const Type &upper) {
+    _interval.assign(lower, upper);
+}
+
+template <typename Type>
+Type Quantity<Type>::getLower() const {
+    return _interval.lower();
+}
+
+template <typename Type>
+Type Quantity<Type>::getUpper() const {
+    return _interval.upper();
+}
+
+template <typename Type>
+Quantity<Type>::IntervalType<Type> Quantity<Type>::getInterval() const {
+    return _interval;
+}
+
+template <typename Type>
+std::string Quantity<Type>::toString()const{
+    std::string s = std::to_string(_value) + " ["
+        + std::to_string(_interval.lower()) + " ; "
+        + std::to_string(_interval.upper()) + " ] ";
+    return s;
+}
+
+}} // namespace snmpp
 
 // Quantity.hpp ends here
