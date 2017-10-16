978ad899290c3e4221667c93febf9a3d96f3ab9b
ABiguet <alexandre.biguet@gmail.com>
2017-10-17 00:11:59 +0200

    add math core variable class

--- src/snmpp/Math/Core/Variable.hpp
+++ src/snmpp/Math/Core/Variable.hpp
@@ -11,6 +11,18 @@
 /******************************************************************************
  * Description: 
  * ------------
+ * This class represents a variable as used in a mathematical function.
+ * A snmpp variable can be fixed or a parameter or a running-variable :
+ * f(x,a,b) = ax + b
+ * x, a and b are snmpp variables. You may want to solve f(x,a,b) = 0 for 'x'
+ * at 'a' and 'b' fixed or you may want to solve f = 0 for 'b' at 'x' and 'a'
+ * fixed.
+ *
+ * When a variable is of parameter type, it means that it is fixed when
+ * solving e.g. f(x,p) = 0. But it is not fixed e.g. when finding p in a chi^2
+ * minimization.
+ *
+ * A snmpp::variable is a snmpp::quantity for which a type has been added.
  ******************************************************************************/
 
 /******************************************************************************
@@ -33,10 +45,71 @@
  * ----------------------
  ******************************************************************************/
 
-namespace  snmpp {
+#include "Quantity.hpp"
+
+namespace snmpp { namespace math {
+
+enum class VariableType {Running, Fixed, Parameter};
+
+template <typename ValueType>
+class Variable : public snmpp::math::Quantity<ValueType> {
+
+  private:
+
+    VariableType _type;
+
+  public:
+    template<typename... Args>
+    explicit Variable( VariableType type, Args...args );
+
+    VariableType getType() const;
+    void setType(VariableType type);
+
+    std::string typeToString()const;
+    std::string toString() const override ;
 
 
+};
 
-} // namespace snmpp 
+template <typename ValueType>
+template<typename... Args>
+Variable<ValueType>::Variable( VariableType type, Args... args ) :
+    Quantity<ValueType>(std::forward<Args>(args)...), _type(type) {}
+
+template<typename ValueType>
+VariableType Variable<ValueType>::getType() const {
+    return _type;
+}
+
+template<typename ValueType>
+void Variable<ValueType>::setType(VariableType type) {
+    _type = type;
+}
+
+template<typename ValueType>
+std::string Variable<ValueType>::typeToString() const {
+    std::string s;
+    switch (_type) {
+        case VariableType::Running :
+            s = "Running";
+            break;
+        case VariableType::Fixed:
+            s = "Fixed";
+            break;
+        case VariableType::Parameter:
+            s = "Parameter";
+            break;
+    }
+    return s;
+}
+
+template<typename ValueType>
+std::string Variable<ValueType>::toString() const {
+    std::string s(Quantity<ValueType>::toString());
+    s+=" type : " + typeToString();
+    return s;
+}
+
+}} // namespace snmpp::math
 
 // Variable.hpp ends here
