cd7cb5780b857a3ce3cd6c83d9f8d814e1682af1
ABiguet <alexandre.biguet@gmail.com>
2017-10-19 22:56:33 +0200

    math core variables : implementation done

--- src/snmpp/Math/Core/Variables.hpp
+++ src/snmpp/Math/Core/Variables.hpp
@@ -42,9 +42,8 @@
 #include <map>
 #include <string>
 #include <tuple>
-//#include <type_traits>
-//#include <utility>
 #include <vector>
+#include <deque>
 
 #include "Variable.hpp"
 
@@ -53,22 +52,30 @@ namespace snmpp { namespace math {
 template <typename Key, typename ValueType>
 class Variables{
 
+  public:
+
+    using VariablesContainer = std::map<Key, Variable<ValueType> >;
+    using iterator = typename VariablesContainer::iterator;
+    using const_iterator = typename VariablesContainer::const_iterator;
+    using reverse_iterator = typename VariablesContainer::reverse_iterator;
+    using const_reverse_iterator = typename VariablesContainer::const_reverse_iterator;
+
   private:
 
-    std::map<Key, Variable<ValueType> > _vars;
+    VariablesContainer _vars;
+    using OtherVarContainer = std::vector<std::tuple<Key,ValueType,VariableType>>;
 
   public:
 
-    using iterator = typename std::map<Key,Variable<ValueType>>::iterator;
-    using const_iterator = typename std::map<Key, Variable<ValueType>>::const_iterator;
 
-    explicit Variables(const std::map<Key, Variable<ValueType>> &vars);
-    explicit Variables(const std::vector<std::tuple<Key,VariableType, ValueType>>&vars);
+    explicit Variables(const VariablesContainer &vars);
+    explicit Variables(const OtherVarContainer &vars);
+
 
     iterator add(const Key &k, const Variable<ValueType> &v);
 
-    std::map<Key,Variable<ValueType>> get() const;
-    Variable<ValueType> get(const Key &k) const;
+    VariablesContainer get() const;
+    Variable<ValueType> get(const Key &k) const; // may throw
 
 
     /* std container interface stuff */
@@ -78,8 +85,16 @@ class Variables{
     iterator find(const Key &k);
 
     const_iterator begin() const;
-    const_iterator end()const;
-    const_iterator find(const Key &k) const;
+    const_iterator end() const;
+
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+    reverse_iterator rbegin();
+    reverse_iterator rend();
+
+    const_reverse_iterator crbegin() const;
+    const_reverse_iterator crend() const;
 
     bool empty() const;
     iterator erase(const_iterator it);
@@ -87,7 +102,6 @@ class Variables{
     void clear();
     std::size_t size();
 
-  private :
 
 };
 
@@ -95,20 +109,14 @@ class Variables{
  * Implementation
  ******************************************************************************/
 
-/**
- *
- * @tparam Key : key type
- * @tparam ValueType  : value type
- * @param vars : a std map of variables
- */
+template <typename Key, typename ValueType>
+Variables<Key,ValueType>::Variables(const VariablesContainer &vars)
+    : _vars(vars)
+{}
+
 
 template <typename Key, typename ValueType>
-Variables<Key,ValueType>::Variables
-    (const std::map<Key, Variable<ValueType>> &vars) : _vars(vars){}
-
-template <typename Key, typename ValueType>
-Variables<Key,ValueType>::Variables(const std::vector<
-    std::tuple<Key, VariableType, ValueType>> & vars) {
+Variables<Key,ValueType>::Variables(const OtherVarContainer & vars) {
 
     for( const auto &i : vars ){
 
@@ -116,10 +124,123 @@ Variables<Key,ValueType>::Variables(const std::vector<
                        Variable<ValueType>(std::get<1>(i), std::get<2>(i)));
 
         if ( !p.second ){
+
             throw std::logic_error("In construction of Variables : another "
                                        "element with same key already exists");
         }
     }
+}
+
+
+template <typename Key, typename ValueType>
+typename Variables<Key,ValueType>::iterator Variables<Key,ValueType>::add
+    (const Key &k, const Variable<ValueType>& v) {
+
+    auto p = _vars.emplace( k, v);
+
+    if ( !p.second ){
+
+        throw std::logic_error("adding a new variable to Variables: another "
+                                   "element with same key already exists");
+    }
+
+    return p.first;
+}
+
+template <typename Key, typename ValueType>
+typename Variables<Key,ValueType>::VariablesContainer
+Variables<Key,ValueType>::get() const {
+
+    return _vars;
+}
+
+template <typename Key, typename ValueType>
+Variable<ValueType> Variables<Key,ValueType>::get(const Key &k) const {
+
+    return _vars.at(k);
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::iterator Variables<K,V>::begin() {
+    return _vars.begin();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::iterator Variables<K,V>::end() {
+    return _vars.end();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::const_iterator Variables<K,V>::begin() const{
+    return _vars.begin();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::const_iterator Variables<K,V>::end() const{
+    return _vars.end();
+}
+
+
+template <typename K, typename V>
+typename Variables<K,V>::iterator Variables<K,V>::find(const K &k) {
+    return _vars.find(k);
+}
+
+
+template <typename K, typename V>
+typename Variables<K,V>::const_iterator Variables<K,V>::cbegin() const {
+    return _vars.cbegin();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::const_iterator Variables<K,V>::cend() const {
+    return _vars.cend();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::reverse_iterator Variables<K,V>::rbegin() {
+    return _vars.rbegin();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::reverse_iterator Variables<K,V>::rend() {
+    return _vars.rend();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::const_reverse_iterator Variables<K,V>::crbegin() const {
+    return _vars.crbegin();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::const_reverse_iterator Variables<K,V>::crend() const {
+    return _vars.crend();
+}
+
+template <typename K, typename V>
+bool Variables<K,V>::empty() const {
+    return _vars.empty();
+}
+
+template <typename K, typename V>
+typename Variables<K,V>::iterator
+Variables<K,V>::erase(typename Variables<K,V>::const_iterator it) {
+    return _vars.erase(it);
+}
+
+template <typename K, typename V>
+std::size_t Variables<K,V>::erase(const K &k){
+    return _vars.erase(k);
+}
+
+template <typename K, typename V>
+void Variables<K,V>::clear() {
+    _vars.clear();
+}
+
+template <typename K, typename V>
+std::size_t Variables<K,V>::size() {
+    return _vars.size();
 }
 
 }} // namespace snmpp::math
