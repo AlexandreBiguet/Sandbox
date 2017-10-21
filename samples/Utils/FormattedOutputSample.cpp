1860a94e9deb02e0490cb234cdad9029199fd92d
ABiguet <alexandre.biguet@gmail.com>
2017-10-21 17:53:03 +0200

    add sample for formatted sample class

---
+++ samples/Utils/FormattedOutputSample.cpp
@@ -0,0 +1,31 @@
+//
+//  FormattedOutputSample.cpp 
+//  snmpp
+//
+//  Created on 21/10/2017 at 17:48.
+//  Copyright Alexandre Biguet © 2017. All rights reserved.
+//  Distributed under the MIT Licence
+//
+
+#include <iostream>
+#include <snmpp/Utils/FormattedOutput.hpp>
+
+int main ( ){
+
+    double v = 5.0;
+    int n = 10;
+
+    std::cout<<"without formatted format : \n";
+    std::cout<<n<<" "<<v<<"\n"<<v<<" "<<n<<"\n";
+
+    std::cout<<"with formatted format : \n";
+
+    snmpp::utils::FormattedOutput c;
+
+    std::cout<<c<<n<<v<<"\n"<<c<<v<<n<<std::endl;
+
+
+    return 0;
+}
+
+// FormattedOutputSample.cpp ends here
