fb8399ca79e063d20af8e57f7cd95eaee1916601
ABiguet <alexandre.biguet@gmail.com>
2017-10-21 17:48:21 +0200

    add utils formatted output class

---
+++ src/snmpp/Utils/FormattedOutput.hpp
@@ -0,0 +1,64 @@
+//
+//  FormattedOutput.hpp 
+//  snmpp
+//
+//  Created on 21/10/2017 at 17:32.
+//  Copyright Alexandre Biguet © 2017. All rights reserved.
+//  Distributed under the MIT Licence
+//
+#pragma once
+
+/******************************************************************************
+ * Description: 
+ * ------------
+ * A class for formatted output
+ ******************************************************************************/
+
+/******************************************************************************
+ * Class Attributes:
+ * ----------------- 
+ ******************************************************************************/
+
+/******************************************************************************
+ * Public Methods: 
+ * ---------------
+ ******************************************************************************/
+
+/******************************************************************************
+ * Private Methods:
+ * ----------------
+ ******************************************************************************/
+
+/******************************************************************************
+ * Possible enhancements:
+ * ----------------------
+ ******************************************************************************/
+
+namespace snmpp { namespace utils {
+
+class FormattedOutput {
+
+  private:
+    int _width;
+    int _precision;
+
+  public:
+    FormattedOutput(int width = 15, int precision = 6)
+        : _width(width), _precision(precision) {}
+
+
+    friend std::ostream& operator<<
+        ( std::ostream& dest, FormattedFormat const& fmt )
+    {
+        dest.setf( std::ios_base::fixed, std::ios_base::floatfield );
+        dest.setf( std::ios_base::left, std::ios_base::adjustfield );
+        dest.precision( fmt._precision );
+        dest.width( fmt._width );
+        return dest;
+    }
+
+};
+
+}} // namespace snmpp::utils
+
+// FormattedOutput.hpp ends here
