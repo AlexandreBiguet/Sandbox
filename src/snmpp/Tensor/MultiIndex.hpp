cdc81277a558f8d83d305830a7e7624c3aa13f6c
ABiguet <alexandre.biguet@gmail.com>
2017-11-02 00:27:27 +0100

    add tensor multi index class : integral type overload

---
+++ src/snmpp/Tensor/MultiIndex.hpp
@@ -0,0 +1,176 @@
+//
+//  MultiIndex.hpp 
+//  snmpp
+//
+//  Created on 01/11/2017 at 21:42.
+//  Copyright Alexandre Biguet © 2017. All rights reserved.
+//  Distributed under the MIT Licence
+//
+#pragma once
+
+/* Structure permettant d'obtenir par suite d'appel à multi_index_next
+   toutes les possibilités de dim indices chacun allant de 0 à
+   Nmax[i]-1
+
+   Cette stucture permet de simuler dim boucles for imbriquées les
+   unes dans les autres.
+*/
+
+/******************************************************************************
+ * Description: 
+ * ------------
+ * Let's consider a vector of type T of fixed size.
+ * Each element of the vector is allowed to run from 'begin' to 'end'.
+ *
+ * The MultiIndex class allows to get every combination of possible values.
+ *
+ * For example:
+ * Let v be a vector of integer type of fixed size dim
+ * Each element of the vector can run from 0 to N_i - 1, where N_i is the
+ * maximum value that the i-th element can reach
+ * By successive call of the next() method we get :
+ * ( 0, 0, ..., 0) -> (0, 0, ..., 1) -> ... -> ( 0, 1, ..., 0) -> (0, 1, ..., 1)
+ * -> ... -> (1, 1, ..., 1) -> ... -> (N_0-1, N_1 -1, ..., N_[dim-1] - 1)
+ *
+ * The MultiIndex Class is templated such that it accepts iterator as element
+ * of v or integer type.
+ *
+ * In the case of iterator, N_i = it.end()
+ *
+ * In the implementation, N is called _Nmax
+ *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Possible enhancements:
+ * ----------------------
+ ******************************************************************************/
+
+#include <vector>
+#include <type_traits>
+
+namespace snmpp { namespace tensor {
+
+template < typename T, typename Enable = void>
+class MultiIndex {};
+
+
+/**
+ * Specialization for integer template parameter
+ * */
+template < typename T >
+class MultiIndex<T, typename std::enable_if<std::is_integral<T>::value>::type> {
+
+    /***************************************************************************
+     * Interface
+     **************************************************************************/
+
+  public:
+
+    /**
+     * Constructor
+     * */
+    explicit MultiIndex( const std::vector<T> &Nmax )
+        : _Nmax(Nmax), _Ntot(1), _Ncur(0) {
+
+        for( const auto &i : _Nmax ){
+            _Ntot *= i;
+        }
+
+        _dim = _Nmax.size();
+        _data.resize(_dim);
+
+    }
+
+    /**
+     * Computes the next possible combination of indexes.
+     * Returns true if other combinations can be computed.
+     * Returns false if the last combination was computed
+     * */
+    bool next() {
+
+        if ( _Ncur == _Ntot - 1){
+            return false;
+        }
+
+        _data[_dim - 1] += 1;
+
+        std::size_t i = 1;
+
+        while ( _data[_dim - i] == _Nmax[ _dim - i ] ){
+
+            _data[ _dim - i     ]  = 0;
+            _data[ _dim - i - 1 ] += 1;
+
+            ++i;
+        }
+
+        ++_Ncur;
+        return true;
+    }
+
+    /**
+     * Returns a copy of the i-th element of the current combination
+     * */
+    T get( std::size_t i ) const {
+        return _data[i];
+    }
+
+    /***************************************************************************
+     * Private
+     **************************************************************************/
+
+  private:
+
+    /**
+     * This vector holds the current value of each element of the vector
+     * */
+    std::vector<T> _data;
+
+    /**
+     * This vector holds the maximum value that each element can reach
+     * */
+    std::vector<T> _Nmax;
+
+    /**
+     * This is the number of possible combination of each value. It is
+     * defined by the sum of all _Nmax[i] value
+     * */
+    std::size_t _Ntot;
+
+    /**
+     * Holds the (constant) size of the input Nmax vector
+     * */
+    std::size_t _dim;
+
+    /**
+     * Holds the current index (goes from 0 to _dim - 1)
+     * */
+    std::size_t _Ncur;
+
+};
+
+
+/**
+ * Specialization for non integral template parameter.
+ * Template parameter must be an iterator
+ * */
+template < typename T >
+class MultiIndex<T, typename std::enable_if<!std::is_integral<T>::value>::type> {
+
+    /**
+     * Interface
+     * */
+  public:
+
+    /**
+     * Private
+     * */
+  private:
+
+};
+
+
+}} // namespace snmpp::tensor
+
+// MultiIndex.hpp ends here
