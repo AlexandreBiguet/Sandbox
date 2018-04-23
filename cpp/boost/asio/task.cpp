cbf2fa9c2b858f5341a9bc6b14dc287f2df04d09
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-04-23 11:17:52 +0200

    update repository tree + add cpp sandbox tree

---
+++ cpp/boost/asio/task.cpp
@@ -0,0 +1,18 @@
+//
+//  task.cpp 
+//  Sandbox
+//
+//  Created by Alexandre Biguet on 23/04/2018 at 11:09.
+//  Copyright © 2018 UAVIA. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "boost asio io_service task binder \n";
+
+  return 0;
+}
+
+// task.cpp ends here
