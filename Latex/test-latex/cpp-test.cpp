b9df82c4c9fb85ddaabad9a9b158cde6d99103fc
ABiguet <alexandre.biguet@gmail.com>
2018-05-13 11:50:58 +0200

    add legacy latex

---
+++ Latex/test-latex/cpp-test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std ;
+
+int main ()
+{
+  double a ;
+  string s ;
+  char c ;
+  return 0 ;
+}
+
+void funct ( std::string a );
---
+++ Latex/test-tikz/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc , char * argv[] )
+{
+  char w[1024] ;
+  sprintf ( w , 
+	    "\\documentclass{report} \n\n"
+	    "\\usepackage{tikz}\n\n"
+	    "\\begin{document}\n\n"
+	    "\\end{document}\n" );
+  //printf ("%s",w);
+  FILE * f = NULL ;
+  int i ;
+  for ( i = 1 ; i<argc ; i++ )
+    {
+      f = fopen ( argv[i] , "w");
+      if ( f == NULL )
+	{
+	  printf ("Probleme ouverture fichier %s \n",argv[i] );
+	  exit(1);
+	}
+      fprintf ( f , "%s",w );
+      fclose ( f ); 
+    }
+
+  
+
+  return 0; 
+}
+
