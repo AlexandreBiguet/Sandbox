23d3dcab1ca2c07c678043cf84abd5ad2e186387
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-05-27 11:03:52 +0200

    testing cmake_format

---
+++ cmake/cmake_format/src/hello.cpp
@@ -0,0 +1,14 @@
+/**
+ * @Author: Alexandre Biguet <alexandrebiguet>
+ * @Date:   27 - May - 2018
+ * @Last modified by:   alexandrebiguet
+ * @Last modified time: 27 - May - 2018
+ */
+
+#include "hello.hpp"
+
+#include <iostream>
+
+namespace Test {
+void test() { std::cout << "this is a test \n"; }
+}  // namespace Test
---
+++ cmake/cmake_format/src/hello.hpp
@@ -0,0 +1,12 @@
+/**
+ * @Author: Alexandre Biguet <alexandrebiguet>
+ * @Date:   27 - May - 2018
+ * @Last modified by:   alexandrebiguet
+ * @Last modified time: 27 - May - 2018
+ */
+
+#pragma once
+
+namespace Test {
+void test();
+}
