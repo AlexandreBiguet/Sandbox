801d5778819bda6f800eb851da9b01f8c0289e83
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-06-21 13:01:36 +0200

    update nlohmann json test

--- cpp/nlohmann_json/main.cpp
+++ cpp/nlohmann_json/main.cpp
@@ -34,12 +34,29 @@ struct Two {
   Tensor3d<double> tensor3d() const { return {matrix()}; }
 };
 
+/**
+ * Will it work to just json[] = "class -> std::vector".instance() ?
+ * --> YEAH ! :-)
+ */
+template <typename T>
+struct MyVector : public std::vector<T, std::allocator<T>> {
+  using base_t = std::vector<T, std::allocator<T>>;
+  MyVector() : base_t() {}
+};
+
 int main() {
   nlohmann::json json;
   Two two;
   json["vector"] = two.vector();
   json["matrix"] = two.matrix();
   json["tensor3d"] = two.tensor3d();
+
+  MyVector<double> vec;
+  vec.push_back(1.0);
+  vec.push_back(2.0);
+  vec.push_back(3.0);
+  json["myVector"] = vec;
+
   std::cout << json << std::endl;
   return 0;
 }
