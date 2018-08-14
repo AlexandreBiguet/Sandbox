bac0c9ad28db25e420ef858201e40b6d25e38484
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-08-14 10:46:10 +0200

    still playing with numpy

--- numpy/array_creation.py
+++ numpy/array_creation.py
@@ -3,6 +3,7 @@ array creation
 
 from : https://docs.scipy.org/doc/numpy/reference/routines.array-creation.html
 
+no brain, just following the routine list
 
 """
 
@@ -15,7 +16,9 @@ _line_width = 80
 def func_name_printer(the_function):
     """
     decorator used to print split functions calls
+
     :param the_function: function to be decorated
+
     :return: the wrapped function
     """
     def wrap():
@@ -100,6 +103,7 @@ def np_identity():
     """
     using numpy.identity(n, dtype=None)[source]
           returns the identity array.
+
     :return: None
     """
     identity = np.identity(5, dtype=int)
@@ -111,6 +115,7 @@ def np_ones():
     """
     using numpy.ones(shape, dtype=None, order='C')
           returns a new array of given shape and type, filled with ones.
+
     :return: None
     """
 
@@ -122,6 +127,7 @@ def np_ones():
 
     ones = np.ones((5, 1), dtype=int)
     print('col array of ones : \n', ones, '\n')
+
 
 @func_name_printer
 def np_ones_like():
@@ -142,12 +148,161 @@ def np_ones_like():
           'ones: \n', np.ones_like(arr), '\n')
 
 
+@func_name_printer
+def np_zeros():
+    """
+    using numpy.zeros(shape, dtype=float, order='C')¶
+          Return a new array of given shape and type, filled with zeros.
+
+    :return: None
+    """
+    zer = np.zeros(5)
+    print('line array of zeros : \n', zer, '\n')
+
+    zer = np.zeros((5, 3))
+    print('array (shape', zer.shape, ') : \n', zer, '\n')
+
+
+@func_name_printer
+def np_zeros_like():
+    """
+    using numpy.zeros_like(a, dtype=None, order='K', subok=True)
+          Return an array of zeros with the same shape and type as a
+          given array.
+
+    :return: None
+    """
+
+    arr = np.arange(6).reshape((3, 2))
+    print('input array : \n', arr, '\n')
+
+    ones = np.zeros_like(arr)
+    print('one like : \n', ones, '\n')
+
+    arr = np.arange(10)
+    print('also works with line array: \n', arr, '\n',
+          'ones: \n', np.zeros_like(arr), '\n')
+
+
+@func_name_printer
+def np_full():
+    """
+    using numpy.full(shape, fill_value, dtype=None, order='C')
+          Return a new array of given shape and type, filled with fill_value.
+
+    :return: None
+    """
+
+    value = 10
+    arr = np.full(5, value, dtype=int)
+    print('line array filled with ', value, '\n', arr, '\n')
+
+    arr = np.full(5, value, dtype=np.float16)
+    print('same array but float type ', value, '\n', arr, '\n')
+
+    value = np.nan
+    arr = np.full((5, 5), value, dtype=float)
+    print('array shape: ', arr.shape, ' filled with ', value, '\n', arr, '\n')
+
+
+@func_name_printer
+def np_full_like():
+    """
+    using numpy.full_like(a, fill_value, dtype=None, order='K', subok=True)
+          Return a full array with the same shape and type as a given array.
+
+    :return: None
+    """
+
+    arr = np.arange(0.0, 20.0, 2).reshape(2, 5)
+    print('init array (dtype :', arr.dtype, ') \n', arr, '\n')
+
+    value = 10
+    ful = np.full_like(arr, value, dtype=np.int8)
+    print('full_like array (dtype :', ful.dtype,
+          ') (value : ', value, ') \n', ful, '\n')
+
+    print('what happens if value is > than max of type ?'
+          ' type change or cycle ? ')
+    value = 128
+    ful = np.full_like(arr, value, dtype=np.int8)
+    print('full_like array (dtype :', ful.dtype,
+          ') (value : ', value, ') \n', ful, '\n')
+
+
+@func_name_printer
+def np_array():
+    """
+    using
+    numpy.array(object, dtype=None, copy=True, order='K', subok=False, ndmin=0)
+    Create an array.
+
+    :note: see the doc for more information about parameters
+
+    :return: None
+    """
+    arr = np.array([1, 2, 3])
+    print('array creation from list: \n', arr, '\n')
+
+    arr = np.array([10, 20, 30.0])
+    print('array creation (upcasting) to float \n', arr, '\n')
+
+    arr = np.array([[1, 2, 3], [4, 5, 6]])
+    print('two dim. array \n', arr, '\n')
+
+    arr = np.array([1, 2, 3, 4], ndmin=2)
+    print('creation of an array with minimum 2 dimensions : \n', arr, '\n')
+
+    arr = np.array([1, 2, 3, 4], ndmin=4, dtype=complex)
+    print('creation of an array with minimum 4 dimensions (dtype :',
+          arr.dtype, ') : \n', arr, '\n')
+
+    # subok parameter:
+    # If True, then sub-classes will be passed-through, otherwise the returned
+    # array will be forced to be a base-class array (default).
+
+    arr = np.array(np.mat('1 2 ; 3 4'), subok=False)
+    print('creation of an array from a array subclass (matrix).\nArray type : ',
+          type(arr), '\n', arr, '\n')
+
+    arr = np.array(np.mat('1 2 ; 3 4'), subok=True)
+    print('creation of an array from a array subclass (matrix).\nArray type : ',
+          type(arr), '\n', arr, '\n')
+
+
+@func_name_printer
+def np_asarray():
+    a_list = [1, 2]
+    arr = np.asanyarray(a_list)
+    print('input list : ', a_list, ' as numpy array : ', arr, '\n')
+
+    a_list_of_list = [[1, 2, 3], [4, 5, 6]]
+    arr = np.asanyarray(a_list_of_list)
+    print('input list of list : \n', a_list_of_list,
+          '\n', 'as a np array: \n', arr, '\n')
+
+    a_tuple = (1, 2, 3)
+    arr = np.asanyarray(a_tuple)
+    print('input tuple : ', a_tuple, 'as a numpy array : ', arr, '\n')
+
+    a_tuple_of_tuple = ((1, 2), (3, 4))
+    arr = np.asanyarray(a_tuple_of_tuple)
+    print('input tuple of tuple : \n', a_tuple_of_tuple,
+          '\n', 'as a np array: \n', arr, '\n')
+
+
 def main():
     np_empty()
     np_empty_like()
     np_eye()
     np_ones()
     np_ones_like()
+    np_zeros()
+    np_zeros_like()
+    np_full()
+    np_full_like()
+    np_array()
+    np_asarray()
 
 
 if __name__ == '__main__':
