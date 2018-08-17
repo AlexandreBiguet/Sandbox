2c0655a4112c9780c14802e3178d6278507aa74a
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-08-17 10:14:43 +0200

    uopdate opencv readme + playing with opencv border types

--- opencv/README.md
+++ opencv/README.md
@@ -9,4 +9,5 @@ Contents :
 - The `samples` directory contains examples from the book (more or less modified)
 - The `sandbox` directory contains some very small tests. 
 - The `doc` directory contains some tutos from the openCV doc
+- The `python` directory contains opencv-python code
 Note : More informations about the tests and samples in the code.
--- opencv/python/e10.py
+++ opencv/python/e10.py
@@ -19,19 +19,92 @@ def main():
     source = cv2.imread(img_path, cv2.IMREAD_COLOR)
     source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
 
-    top = 10
-    bottom = 10
-    left = 10
-    right = 10
+    lenght = 100
+    top = lenght
+    bottom = lenght
+    left = lenght
+    right = lenght
+
+    border_type_list = (i for i in dir(cv2) if 'BORDER' in i)
+    print('List of border type : ')
+    for btype in border_type_list:
+        print(btype)
+
+    constant = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                  cv2.BORDER_CONSTANT)
+
+    default = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                 cv2.BORDER_DEFAULT)
+
+    isolated = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                  cv2.BORDER_ISOLATED)
+
+    reflect = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                 cv2.BORDER_REFLECT)
+
+    reflect101 = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                    cv2.BORDER_REFLECT101)
+
+    reflect_101 = cv2.copyMakeBorder(source, top, bottom, left, right,
+                                     cv2.BORDER_REFLECT_101)
 
     replicate = cv2.copyMakeBorder(source, top, bottom, left, right,
-                                   cv2.BORDER_REPLICATE,)
+                                   cv2.BORDER_REPLICATE)
 
-    plt.subplot(221)
-    plt.imshow(source, 'gray'), plt.title('original')
+    wrap = cv2.copyMakeBorder(source, top, bottom, left, right,
+                              cv2.BORDER_WRAP)
 
-    plt.subplot(222)
-    plt.imshow(replicate, 'gray'), plt.title('replicate')
+    # pyplot imshow options
+    options = {'cmap': 'gray', 'aspect': 'equal'}
+
+    # Fig. 1
+
+    plt.figure()
+    plt.imshow(source, **options)
+    plt.title('original')
+
+    # Fig. 2
+
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(constant, **options)
+    plt.title('constant')
+
+    plt.subplot(132)
+    plt.imshow(default, **options)
+    plt.title('default')
+
+    plt.subplot(133)
+    plt.imshow(isolated, **options)
+    plt.title('isolated')
+
+    # Fig. 3
+
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(reflect, **options)
+    plt.title('reflect')
+
+    plt.subplot(132)
+    plt.imshow(reflect101, **options)
+    plt.title('reflect101')
+
+    plt.subplot(133)
+    plt.imshow(reflect_101, **options)
+    plt.title('reflect_101')
+
+    # Fig. 4
+
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(replicate, **options)
+    plt.title('replicate')
+
+    plt.subplot(122)
+    plt.imshow(wrap, **options)
+    plt.title('wrap')
+
+    # Plot
 
     plt.show()
 
