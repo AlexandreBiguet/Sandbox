bcaa541cf5efcfccaf21d8c89d6250c8d106996b
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-08-17 09:06:17 +0200

    clean latex directory

---
+++ Latex/README.md
@@ -0,0 +1,16 @@
+# Latex
+
+Contains some (latex) tests performed during my phd thesis and some notes
+
+## Utils
+
+* no output for `pdflatex` : `texfot pdflatex foo.tex`
+
+## Packages
+
+* [titlepage](https://ctan.org/pkg/titlepages)
+* [tikz-feynman](https://ctan.org/pkg/tikz-feynman)
+* [latexdraw](http://latexdraw.sourceforge.net/)
+* [jaxodraw](http://jaxodraw.sourceforge.net/)
+* [geogebra](https://www.geogebra.org/)
+* [feynmf](https://ctan.org/pkg/feynmf)
--- Latex/test-tikz/test.c
+++ Latex/test-tikz/test.c
@@ -1,26 +1,20 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int main ( int argc , char * argv[] )
-{
-  if ( argc < 2 ) 
-    {
-      printf ("exit");
-      return 1;
-    }
-  int i ;
-  int N = atoi ( argv[1] ) ;
-  double m = 0. , M = 5. , x , it ,fx ;
-  it = (M - m)/(double)N ;
-  for ( i = 0  ; i < N ; i++ )
-    {
-      x = m + i * it ;
-      fx = sin (x) ;
-      printf ("%15.5f %15.5f\n" , x , fx );
-    }
-  return 0 ;
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("exit");
+    return 1;
+  }
+  int i;
+  int N = atoi(argv[1]);
+  double m = 0., M = 5., x, it, fx;
+  it = (M - m) / (double)N;
+  for (i = 0; i < N; i++) {
+    x = m + i * it;
+    fx = sin(x);
+    printf("%15.5f %15.5f\n", x, fx);
+  }
+  return 0;
 }
-
-
-
