1fcaa6f83543431ae1febd2f3087e677a824483e
AlexandreBiguet <alexandre.biguet@gmail.com>
2018-09-27 21:22:26 +0200

    update inheritance3

--- cpp/std/inheritance3.cpp
+++ cpp/std/inheritance3.cpp
@@ -1,5 +1,5 @@
 //
-//  inheritance3.cpp 
+//  inheritance3.cpp
 //  Sandbox
 //
 //  Created by Alexandre Biguet on 27/09/2018 at 15:02.
@@ -8,37 +8,43 @@
 #include <memory>
 #include <utility>
 
-template <typename T>
-class Base : public T {
- public:
+// #define PURE_VIRTUAL_BASE
+
+template <typename T> class Base : public T {
+public:
   using Type = T;
   using Ptr = std::shared_ptr<Base>;
   ~Base() = default;
 
-  template <typename ...Args>
-  Base(Args&& ... args) : T(std::forward<Args...>(args...)) { }
+  template <typename... Args>
+  Base(Args &&... args) : T(std::forward<Args...>(args...)) {}
 
-  virtual const T& get() const = 0;
+#ifdef PURE_VIRTUAL_BASE
+  virtual const T &get() const = 0;
+#else
+  const T &get() const { return *this; }
+#endif
 };
 
-template <typename T>
-class Tone {
- public:
-  Tone(const T& t) : _t(t) { }
-  void set(const T& t) { _t = t; }
+template <typename T> class Tone {
+public:
+  Tone(const T &t) : _t(t) {}
+  void set(const T &t) { _t = t; }
   T t() const { return _t; }
- private:
+
+private:
   T _t;
 };
 
-template <typename T>
-class Son1 : public Base <Tone<T>> {
- public:
-  Son1(const T& t) : Base <Tone<T>> (t) { }
-  const Tone<T>& get() const override { return *this; }
+template <typename T> class Son1 : public Base<Tone<T>> {
+public:
+  Son1(const T &t) : Base<Tone<T>>(t) {}
 
- private:
+#ifdef PURE_VIRTUAL_BASE
+  const Tone<T> &get() const override { return *this; }
+#endif
 
+private:
 };
 
 int main() {
