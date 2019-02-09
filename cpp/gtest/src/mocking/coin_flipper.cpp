d50c7590d7a262c206bbf4ddf9629850f421c021
AlexandreBiguet <alexandre.biguet@gmail.com>
2019-02-09 17:54:35 +0100

    update mocking example

--- cpp/gtest/src/mocking/coin_flipper.cpp
+++ cpp/gtest/src/mocking/coin_flipper.cpp
@@ -5,13 +5,23 @@
 namespace mocking {
 
 CoinFlipper::CoinFlipper(Rng* rng) : _rng(rng) {
-    if(!_rng) {
-        throw std::invalid_argument("nullptr argument...");
-    }
+  if (!_rng) {
+    throw std::invalid_argument("nullptr argument...");
+  }
 }
 
 CoinFlipper::Result CoinFlipper::flip() const {
-    return _rng->generate(0.0, 1.0) < 0.5 ? Result::Head: Result::Tail;
+  return _rng->generate(0.0, 1.0) < 0.5 ? Result::Head : Result::Tail;
+}
+
+CoinFlipper2::CoinFlipper2(const std::shared_ptr<Rng>& rng) : _rng(rng) {
+  if (!_rng) {
+    throw std::invalid_argument("nullptr argument...");
+  }
+}
+
+CoinFlipper2::Result CoinFlipper2::flip() const {
+  return _rng->generate(0.0, 1.0) < 0.5 ? Result::Head : Result::Tail;
 }
 
 }  // namespace mocking
--- cpp/gtest/src/mocking/coin_flipper.hpp
+++ cpp/gtest/src/mocking/coin_flipper.hpp
@@ -14,6 +14,17 @@ class CoinFlipper {
   Result flip() const;
 
  private:
+  Rng* _rng;
+};
+
+class CoinFlipper2 {
+ public:
+  enum Result { Head = 0, Tail = 1 };
+
+  CoinFlipper2(const std::shared_ptr<Rng>& rng);
+  Result flip() const;
+
+ private:
   std::shared_ptr<Rng> _rng;
 };
 
