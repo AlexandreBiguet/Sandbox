9bff12412dccdacde001a99e6d6d635beb33dcca
AlexandreBiguet <alexandre.biguet@gmail.com>
2019-03-22 09:17:29 +0100

    playing with hunter cmake package manager

---
+++ cpp/hunter/README.md
@@ -0,0 +1,3 @@
+# Description
+
+Testing [Hunter](https://github.com/ruslo/hunter) CMake package manager
---
+++ cpp/hunter/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <boost/optional.hpp>
+
+int main() {
+  std::cout << "Hello \n";
+  boost::optional<int> opt;
+  return 0;
+}
